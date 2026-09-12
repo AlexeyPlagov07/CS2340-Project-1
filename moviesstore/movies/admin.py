74e58d5dfc6495434176a974ba1941d28980ed2f
AlexeyPlagov07 <137966212+AlexeyPlagov07@users.noreply.github.com>
2026-09-12 19:01:40 -0400

    Last user story
    Implemented the report feature.

--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -3,5 +3,10 @@ from .models import Movie, Review
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'movie', 'user', 'is_reported']
+    list_filter = ['is_reported']
+
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
